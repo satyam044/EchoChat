71163ea439c8d04e60bf6f56d9df3091a997346f
satyam <mishrasatyam9890@gmail.com>
2025-04-30 17:03:45 +0530

    Home Done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HomeL from './HomeL';
 import HomeR from './HomeR';
-import { FaGoogle, FaAmazon, FaWordpress, FaShopify, } from "react-icons/fa";
+import { FaGoogle, FaAmazon, FaWordpress, FaShopify } from "react-icons/fa";
 import { SiDell } from "react-icons/si";
 
 const Home = () => {
@@ -14,14 +14,16 @@ const Home = () => {
     ];
 
     return (
-        <div className='Home w-full h-[100dvh] flex relative'>
+        <div className='Home w-full h-[100dvh] flex relative overflow-hidden'>
             <h4 className='h-full w-full text-9xl z-0 border-l-[20px] border-r-[20px] border-black text-transparent font-extrabold absolute flex justify-center items-center'>EchoChat</h4>
-            <HomeL/>
-            <HomeR/>
-            <div className='wrapper w-full z-50 p-6 flex whitespace-nowrap bg-linear-to-r from-[#0d1e2a] to-[#0095f0] absolute bottom-16 border-t-2 border-b-2 -rotate-2'>
-                <div className='w-full relative flex items-center'>
-                    {sliderArray.concat(sliderArray).map((item, index) => (
-                        <div key={index} className={`slide item${index + 1} cursor-pointer absolute left-[120%] flex items-center gap-2 text-2xl`}>{item.svg}{item.name}</div>
+            <HomeL />
+            <HomeR />
+            <div className='wrapper w-full z-50 p-2 bg-gradient-to-r from-[#0d1e2a] to-[#0095f0] absolute bottom-16 border-t-2 border-b-2 -rotate-2 overflow-hidden'>
+                <div className='slider flex gap-30 animate-scroll'>
+                    {sliderArray.map((item, index) => (
+                        <div key={index} className='flex items-center gap-2 text-2xl whitespace-nowrap'>
+                            {item.svg} {item.name}
+                        </div>
                     ))}
                 </div>
             </div>
